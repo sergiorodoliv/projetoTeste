188aa9a0c9ccb9dc34088e7907eb19cddf8f3e1f
sivin <sivin@LAPTOP-MQP7JIVM>
2023-12-04 21:31:13 -0200

    Criação Classe Aluno

---
+++ projetoTeste/src/classes/Aluno.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String RegistroGeral;
+	String numeroCpf;
+	String dataNascimento;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+
+}
