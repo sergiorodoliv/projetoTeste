22db7791608bf3e945ea64e22490371029cee109
sivin <sivin@LAPTOP-MQP7JIVM>
2023-12-05 01:15:28 -0200

    Criação dialog object (Lista)

--- projetoTeste/src/projetoTeste/projetoEntradaDados.java
+++ projetoTeste/src/projetoTeste/projetoEntradaDados.java
@@ -6,18 +6,36 @@ public class projetoEntradaDados {
 	
 	public static void main(String[] args) {
 		
-		String carros = JOptionPane.showInputDialog("DIgite um numero: ");
+		//String carros = JOptionPane.showInputDialog("DIgite um numero: ");
 		
 		//System.out.println("Carro é " + carros);
 		
-		int resposta = JOptionPane.showInternalConfirmDialog(null, "Deseja ver o Carro Digitado?");
+		//int resposta = JOptionPane.showInternalConfirmDialog(null, "Deseja ver o Carro Digitado?");
 		
 	//	System.out.println(resposta);
-		if (resposta == 0) {
+		//if (resposta == 0) {
 			
-			JOptionPane.showMessageDialog(null, "Carro é " + carros);
+		//	JOptionPane.showMessageDialog(null, "Carro é " + carros);
+			
+		//}
+		
+		Object[] op = {"Masculino","Feminino"};
+		////Cria lista de valores
+		String resp = (String) JOptionPane.showInputDialog(null,"Selecione o sexo:\n","Pesquisa", 
+				JOptionPane.PLAIN_MESSAGE,null,op,"Feminino");
+		
+		if (resp == null) {
+			JOptionPane.showMessageDialog(null, "Você pressionou cancel");
+		}
+		if (resp.equals("Masculino")) {
+			JOptionPane.showMessageDialog(null,"Você é homem");
 			
 		}
+		if (resp.equals("Feminino")) {
+			JOptionPane.showMessageDialog(null,"Você é Mulher");
+			
+		}
+		
 		
 		
 	}
