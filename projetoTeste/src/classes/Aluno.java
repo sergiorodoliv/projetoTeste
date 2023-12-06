ee01bfc0a8e8e6956d6b26eab2d73c26c5151e5e
sivin <sivin@LAPTOP-MQP7JIVM>
2023-12-06 02:37:45 -0200

    Trabalando com metodos

--- projetoTeste/src/classes/Aluno.java
+++ projetoTeste/src/classes/Aluno.java
@@ -13,6 +13,12 @@ public class Aluno {
 	String nomeEscola;
 	String serieMatriculado;
 	
+	double nota1;
+	double nota2;
+	double nota3;
+	double nota4;
+	
+
 	public Aluno() {
 		
 	}
@@ -21,4 +27,129 @@ public class Aluno {
 		nome = nomePadrao;
 	}
 
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getRegistroGeral() {
+		return RegistroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		RegistroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+
+	public double getMediaNota() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+	
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaNota();
+		if (media >= 70) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
 }
--- projetoTeste/src/projetoTeste/programaExecutavel.java
+++ projetoTeste/src/projetoTeste/programaExecutavel.java
@@ -5,12 +5,28 @@ import classes.Aluno;
 public class programaExecutavel {
 	
 	public static void main (String[] args) {
-
+        /*aluno1 é uma referencia do obbjeto Aluno, new Aluno() é uma instancia*/
 		Aluno  aluno1 = new Aluno();
 		
-		Aluno aluno2 = new Aluno("Sérgio");
+		aluno1.setDataMatricula("05/12/2023");
+		aluno1.setDataNascimento("08/04/1965");
+		aluno1.setIdade(58);
+		aluno1.setNome("Sérgio Rodrigues");
+		aluno1.setNomeEscola("Curso Java");
+		aluno1.setNomeMae("Vicentina");
+		aluno1.setNomePai("Domingos");
+		aluno1.setNumeroCpf("001.002.0033-90");
+		aluno1.setRegistroGeral("14.456.789");
+		aluno1.setSerieMatriculado("Primeiro");
 		
-		System.out.println("Numero dez: ");
+		aluno1.setNota1(70);
+		aluno1.setNota2(70);
+		aluno1.setNota3(75);
+		aluno1.setNota4(70);
+		
+		System.out.println("Media do Aluno "+ aluno1.getNome() + " é " + aluno1.getMediaNota());
+		System.out.println("Aluno está " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		
 	}
 	
 }
